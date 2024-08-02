53efa746f80a80071cc55caa39c3e9ca98f8a57c
colins777 <rumyga@gmail.com>
2024-08-02 23:11:33 +0300

    fixes for Models/fake data added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use App\Models\Message;
+use App\Models\Conversation;
+use App\Models\Group;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +20,53 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
         ]);
+
+        User::factory()->create([
+            'name' => 'Ihor Doe',
+            'email' => 'ihor@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+
+        User::factory(10)->create();
+
+
+        //create Groups
+        for ($i = 0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(1000)->create();
+        //get all messages that targeted for only the users, not groups
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+
+        //group all messages by sender-receiver ID to get array like this
+        //[1_2 => [[1,2], [2, 1]]
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id, $message->receiver_id])
+                ->sort()->implode('_');
+        })->map(function ($groupedMessages) {
+            //return array without keys 1_2
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon(),
+            ];
+        });
+
+        Conversation::insertOrIgnore($conversations->toArray());
+
     }
 }

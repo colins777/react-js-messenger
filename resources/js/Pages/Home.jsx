7ee146f0b8241b5b310c558e3daf9eb3f54684db
colins777 <rumyga@gmail.com>
2024-08-05 19:38:39 +0300

    fixes for routes/Home page component added

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import ChatLayout from "@/Layouts/ChatLayout";
+
+export default function Home({ auth }) {
+    return (
+        <ChatLayout>
+
+
+           Messages
+        </ChatLayout>
+    );
+}

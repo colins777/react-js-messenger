d8ce7b0a0b26fc91e786c9de4320692433004b4b
colins777 <rumyga@gmail.com>
2024-08-24 18:41:11 +0300

    fixed methods byGroup/loadOlder

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -18,7 +18,7 @@ class MessageController extends Controller
         $messages = Message::where('sender_id', auth()->id())
             ->where('receiver_id', $user->id)
             ->orWhere('sender_id', $user->id)
-            ->where('receiver_id', auth()->id)
+            ->where('receiver_id', auth()->id())
             ->latest()
             ->paginate(10)
             ;
@@ -31,12 +31,40 @@ class MessageController extends Controller
 
     public function byGroup(Group $group)
     {
+        $messages = Message::where('group_id', $group->id)
+            ->latest()
+            ->paginate(50)
+        ;
 
+        return inertia('Home', [
+            'selectedConversation' => $group->toConversationArray(),
+            'messages' => MessageResource::collection($messages)
+        ]);
     }
 
     public function loadOlder(Message $message)
     {
+        if ($message->group_id) {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where('group_id', $message->group_id)
+                ->latest()
+                ->paginate(10)
+                ;
+        } else {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where(function ($query) use ($message) {
+                    $query->where('sender_id', $message->sender_id)
+                        ->where('receiver_id', $message->receiver_id)
+                        ->orWhere('sender_id', $message->receiver_id)
+                        ->where('receiver_id', $message->sender_id)
+                    ;
+                })
+                ->latest()
+                ->paginate(10)
+            ;
+        }
 
+        return MessageResource::collection($messages);
     }
 
     public function store(StoreMessageRequest $request)
